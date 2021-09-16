e11bb7878d59c543a1762b5ab2ecf99344e581e7
kirilliliych <89473962+kirilliliych@users.noreply.github.com>
2021-09-17 02:26:40 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <locale.h>
 
+const int SMALLER = -1;
+const int BIGGER  =  1;
 struct String
 {
     char *str;
@@ -24,16 +26,17 @@ size_t GetFileSize(FILE *file);
 struct String *PlacePointers(char *buffer, size_t lines_number);
 void Swap(void *value_1, void *value_2, size_t type_size);
 void QuickSort(void *data, size_t lines_number, size_t type_size, int (*comparator)(const void *, const void *));
-void TextPrint(FILE *file, struct Text *input_text);
+void PrintText(FILE *file, struct Text *input_text);
 void PrintBuffer(FILE *file, struct Text *input_text);
 int DirectComparator(const void *first_string, const void *second_string);
-int ReversedComparator(const void *first_string, const void *second_string);
+int ReverseComparator(const void *first_string, const void *second_string);
 void FreeMemory(struct Text *input_text);
+
 int main(int argc, const char *argv[])
 {
     setlocale(LC_ALL, "ru_RU.cp1251");
 
-    FILE *input = fopen(argv[1], "r");
+    FILE *input = fopen("Onegin.txt", "r");
     if(input == nullptr)
     {
          printf("ERROR, the file was not found");
@@ -45,23 +48,24 @@ int main(int argc, const char *argv[])
 
     TextInput(input, &input_text);
     fclose(input);
-
-    qsort(&input_text.lines, input_text.lines_number, sizeof(struct String), DirectComparator);
-
+    printf("%d\n", __LINE__);
+    qsort(input_text.lines, input_text.lines_number, sizeof(struct String), DirectComparator);
+    printf("%d\n", __LINE__);
     FILE *output = fopen("Text_sorted.txt", "w");
     assert(output != nullptr);
 
     fprintf(output, "Directly sorted text\n\n");
-    TextPrint(output, &input_text);
+    PrintText(output, &input_text);
     fclose(output);
 
-    QuickSort(&input_text.lines, input_text.lines_number, sizeof(struct String), ReversedComparator);
-
+    //QuickSort(&input_text.lines, input_text.lines_number, sizeof(struct String), DirectComparator);
+    qsort(input_text.lines, input_text.lines_number, sizeof(struct String), ReverseComparator);
+    printf("%d\n", __LINE__);
     output = fopen("Text_sorted.txt", "a");
     assert(output != nullptr);
 
     fprintf(output, "\nReverse sorted text\n\n");
-    TextPrint(output, &input_text);
+    PrintText(output, &input_text);
 
     fprintf(output, "\n Original text\n\n");
     PrintBuffer(output, &input_text);
@@ -72,14 +76,14 @@ int main(int argc, const char *argv[])
     return 0;
 }
 
-void TextInput (FILE *file, struct Text *input_text)
+void TextInput(FILE *file, struct Text *input_text)
 {
     assert (file != nullptr);
     assert (input_text != nullptr);
 
     input_text->buffer = CreateBuffer(file, &input_text->lines_number, &input_text->file_size);
+    printf("%d\n", __LINE__);
     input_text->lines = PlacePointers(input_text->buffer, input_text->lines_number);
-
 }
 
 size_t GetFileSize(FILE *file)
@@ -93,28 +97,32 @@ char *CreateBuffer(FILE *file, size_t *lines_number, size_t *file_size)
     assert (file != nullptr);
     assert (lines_number != nullptr);
     assert (file_size != nullptr);
-                                                               // и что тут нужно сделать ++
-    *(file_size)++ = GetFileSize(file);
+
+    *file_size = GetFileSize(file) + 1;
     rewind(file);
 
     char *buffer = (char*) calloc(*file_size, sizeof(char));
 
     fread(buffer, sizeof(char), *file_size, file);
-    *(buffer + (int)(*file_size)) = '\0';                              // не уверен, что в связи с ниженаписанным нужна эта строчка
 
     size_t string_counter = 0;
 
-    for (size_t counting_strings = 0; counting_strings < *file_size; ++counting_strings)
+    for (size_t checking_strings; checking_strings < *file_size; ++checking_strings)
     {
-        if (*(buffer + counting_strings) == '\n')
+        if (*(buffer + checking_strings) == '\n')
         {
             ++string_counter;
         }
+
     }
 
+    *file_size -= string_counter;
+
+    string_counter++;
+
     *lines_number = string_counter;
-    *file_size -= string_counter;                             // warning!!! поправка на размер файла сделана в предположении, что '/r' в конце строки при считывании с помощью fread удаляются, поэтому необходимо из размера файла вычесть количество символов равное количеству строк в файле
-    *(buffer + (int)(*file_size)) = '\0';                             // в том же самом предположении для нахождения конца "полезного" буфера
+
+    *(buffer + *file_size) = '\0';
 
     return buffer;
 }
@@ -123,24 +131,28 @@ struct String *PlacePointers(char *buffer, size_t lines_number)
 {
     assert(buffer != nullptr);
 
-    struct String *strings = (struct String*) calloc(lines_number, sizeof(struct String));
+    struct String *strings = (struct String *) calloc(lines_number, sizeof(struct String));
 
-    char *pointer_begin = buffer;
+    char *pointer_begin = buffer - 1;
     char *pointer_end = buffer;
     size_t counter = 0;
 
     while (*pointer_begin != '\0')
     {
-        while (*pointer_begin != '\n')
+        ++pointer_begin;
+
+        while ((*pointer_begin != '\n') && (*pointer_begin != '\0'))
         {
             ++pointer_begin;
         }
         (strings + counter)->str = pointer_end;
         (strings + counter)->len = pointer_begin - pointer_end;
 
+        ++counter;
         pointer_end = pointer_begin + 1;
-        ++pointer_begin;
     }
+
+    *pointer_begin = '\n';
 
     return strings;
 }
@@ -150,32 +162,30 @@ int DirectComparator (const void *first_string, const void *second_string)
     assert(first_string  != nullptr);
     assert(second_string != nullptr);
 
-    char *first_string_begin  = (((struct String*) first_string) ->str);
-    char *first_string_end    = (((struct String*) first_string) ->str) + (((struct String*) first_string) ->len);
-    char *second_string_begin = (((struct String*) second_string)->str);
-    char *second_string_end   = (((struct String*) second_string)->str) + (((struct String*) second_string)->len);
+    char *first_string_begin  = (((struct String *) first_string) ->str);
+    char *second_string_begin = (((struct String *) second_string)->str);
 
-    while ((!isalpha((int) *first_string_begin))  && (*first_string_begin  != *first_string_end))
+    while (((*first_string_begin)  < 'А') && (*first_string_begin  != '\n'))
     {
         ++first_string_begin;
     }
 
-    while ((!isalpha((int) *second_string_begin)) && (*second_string_begin != *second_string_end))
+    while (((*second_string_begin) < 'А') && (*second_string_begin != '\n'))
     {
         ++second_string_begin;
     }
 
-    while ((*first_string_begin == *second_string_begin) && (*first_string_begin != *first_string_end) && (*second_string_begin != *second_string_end))
+    while ((*first_string_begin == *second_string_begin) && (*first_string_begin != '\n') && (*second_string_begin != '\n'))
     {
         ++first_string_begin;
         ++second_string_begin;
 
-        while ((!isalpha((int) *first_string_begin))  && (*first_string_begin  != ' ') && (*first_string_begin != *first_string_end))
+        while (((*first_string_begin)  < 'А') && (*first_string_begin  != '\n'))
         {
             ++first_string_begin;
         }
 
-        while ((!isalpha((int) *second_string_begin)) && (*second_string_begin != ' ') && (*second_string_begin != *second_string_end))
+        while (((*second_string_begin) < 'А') && (*second_string_begin != '\n'))
         {
             ++second_string_begin;
         }
@@ -184,40 +194,69 @@ int DirectComparator (const void *first_string, const void *second_string)
     return (((int) *first_string_begin) - ((int) *second_string_begin));
 }
 
-int ReversedComparator (const void *first_string, const void *second_string)
+int ReverseComparator (const void *first_string, const void *second_string)
 {
     assert(first_string  != nullptr);
     assert(second_string != nullptr);
 
     char *first_string_begin  = (((struct String*) first_string) ->str);
-    char *first_string_end    = (((struct String*) first_string) ->str) + (((struct String*) first_string) ->len);
+    char *first_string_end    = (((struct String*) first_string) ->str) + (((struct String*) first_string) ->len) - 1;
     char *second_string_begin = (((struct String*) second_string)->str);
-    char *second_string_end   = (((struct String*) second_string)->str) + (((struct String*) second_string)->len);
+    char *second_string_end   = (((struct String*) second_string)->str) + (((struct String*) second_string)->len) - 1;
 
-    while ((!isalpha((int) *first_string_end))  && (*first_string_begin  != *first_string_end))
+    while (((*first_string_end)  < 'А') && (*first_string_end  != *first_string_begin))
     {
         --first_string_end;
     }
 
-    while ((!isalpha((int) *second_string_end)) && (*second_string_begin != *second_string_end))
+    while (((*second_string_end) < 'А') && (*second_string_end != *second_string_begin))
     {
         --second_string_end;
     }
 
-    while ((*first_string_end == *second_string_end) && (*first_string_begin != *first_string_end) && (*second_string_begin != *second_string_end))
+    while ((*first_string_end == *second_string_end) && (*first_string_end != *first_string_begin) && (*second_string_end != *second_string_begin))
     {
         --first_string_end;
         --second_string_end;
 
-        while ((!isalpha((int) *first_string_end))  && (*first_string_end  != ' ') && (*first_string_begin != *first_string_end))
+        while (((*first_string_end)  < 'А') && (*first_string_end  != *first_string_begin))
         {
             --first_string_end;
         }
 
-        while ((!isalpha((int) *second_string_end)) && (*second_string_end != ' ') && (*second_string_begin != *second_string_end))
+        while (((*second_string_end) < 'А') && (*second_string_end != *second_string_begin))
         {
             --second_string_end;
         }
+    }
+
+    if (*first_string_end == *first_string_begin)
+    {
+        if (*second_string_end != *second_string_begin)
+        {
+            return SMALLER;
+        }
+        else
+        {
+            if (*first_string_end == *second_string_end)
+            {
+                return 0;
+            }
+            else
+            {
+                return (((int) *first_string_end) - ((int) *second_string_end));
+            }
+        }
+    }
+
+    if (*second_string_end == *second_string_begin)
+    {
+        return BIGGER;
+    }
+
+    if (*first_string_end != *second_string_end)
+    {
+        return (((int) *first_string_end) - ((int) *second_string_end));
     }
 
     return (((int) *first_string_end) - ((int) *second_string_end));
@@ -257,21 +296,26 @@ void QuickSort(void *data, size_t lines_number, size_t type_size, int (*comparat
     assert(comparator != nullptr);
 
     size_t left = 0;
-    size_t right = (int) lines_number - 1;
-
+    size_t right = lines_number - 1;
+    size_t middle_element_displacement = type_size * lines_number / 2;
+    printf("%d\n", __LINE__);
     while (left <= right)
     {
 
-        while (comparator(((char *) data + type_size * left),  ((char *) data + type_size * lines_number / 2)) < 0)
+        printf("%d\n", __LINE__);
+        while (comparator(((char *) data + type_size * left),  ((char *) data + middle_element_displacement)) < 0)
         {
             ++left;
+            printf("Left = %d, lines_number = %zy\n", left, lines_number);
         }
-
-        while (comparator(((char *) data + type_size * right), ((char *) data + type_size * lines_number / 2)) > 0)
+        printf("%d\n", __LINE__);
+        while (comparator(((char *) data + type_size * right), ((char *) data + middle_element_displacement)) > 0)
         {
-            ++right;
-        }
+            --right;
+            printf("Right = %d, lines_number = %d\n", right, lines_number);
 
+        }
+        printf("%d\n", __LINE__);
         if (left <= right)
         {
             Swap(((char *) data + type_size * left), ((char *) data + type_size * right), type_size);
@@ -280,7 +324,7 @@ void QuickSort(void *data, size_t lines_number, size_t type_size, int (*comparat
             --right;
         }
     }
-
+    printf("%d\n", __LINE__);
     if (right > 0)
     {
         QuickSort((char *) data, right + 1, type_size, comparator);
@@ -293,18 +337,17 @@ void QuickSort(void *data, size_t lines_number, size_t type_size, int (*comparat
 
 }
 
-void TextPrint(FILE *file, struct Text *input_text)
+void PrintText(FILE *file, struct Text *input_text)
 {
     assert(file != nullptr);
     assert(input_text != nullptr);
 
     for (size_t strings_index = 0; strings_index < input_text->lines_number; ++strings_index)
     {
-        for (size_t into_string_index = 0; into_string_index < (input_text->lines)[strings_index].len; ++into_string_index)
+        for (size_t char_index = 0; char_index <= (input_text->lines)[strings_index].len; ++char_index)
         {
-            //fputc(*((input_text->lines)[strings_index].str + into_string_index), file);
+            fputc(*((input_text->lines)[strings_index].str + char_index), file);
         }
-        fputs((input_text->lines)[strings_index].str, file);
     }
 }
 
@@ -322,6 +365,6 @@ void FreeMemory(struct Text *input_text)
     free(input_text->lines);
 
     input_text->buffer = nullptr;
-    input_text->lines = nullptr;
+    input_text->lines =  nullptr;
 }
 
